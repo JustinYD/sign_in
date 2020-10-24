f3d3217e5f101b5559d805016fff5fecd68ec37c
peidong <yangpeidong@yndysc.com>
2020-10-24 16:04:39 +0800

    初始化

---
+++ src/pages/index/index.vue
@@ -0,0 +1,78 @@
+<template>
+  <div @click="clickHandle">
+    {{motto}}
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      motto: 'Hello miniprograme'
+    }
+  },
+
+  methods: {
+    clickHandle (ev) {
+      this.motto = 'fuck!!!!'
+    }
+  },
+
+  created () {
+    // let app = getApp()
+  }
+}
+</script>
+
+<style scoped>
+.userinfo {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+
+.userinfo-avatar {
+  width: 128rpx;
+  height: 128rpx;
+  margin: 20rpx;
+  border-radius: 50%;
+}
+
+.userinfo-nickname {
+  color: #aaa;
+}
+
+.usermotto {
+  margin-top: 150px;
+}
+
+.form-control {
+  display: block;
+  padding: 0 12px;
+  margin-bottom: 5px;
+  border: 1px solid #ccc;
+}
+.all {
+  width: 7.5rem;
+  height: 1rem;
+  background-color: blue;
+}
+.all:after {
+  display: block;
+  content: '';
+  clear: both;
+}
+.left {
+  float: left;
+  width: 3rem;
+  height: 1rem;
+  background-color: red;
+}
+
+.right {
+  float: left;
+  width: 4.5rem;
+  height: 1rem;
+  background-color: green;
+}
+</style>
